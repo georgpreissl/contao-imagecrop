142c55d1bcb6ba4ee2eba5b85f8810ccd0553373
LupusVII <jln.thirion@gmail.com>
2019-04-16 15:12:58 +0200

    Add FR translations

---
+++ languages/fr/default.php
@@ -0,0 +1,70 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * TYPOlight webCMS
+ *
+ * The TYPOlight webCMS is an accessible web content management system that 
+ * specializes in accessibility and generates W3C-compliant HTML code. It 
+ * provides a wide range of functionality to develop professional websites 
+ * including a built-in search engine, form generator, file and user manager, 
+ * CSS engine, multi-language support and many more. For more information and 
+ * additional TYPOlight applications like the TYPOlight MVC Framework please 
+ * visit the project website http://www.typolight.org.
+ *
+ * 
+ *
+ * @author     Georg Preissl <http://www.georg-preissl.at> 
+ * @package    imagecrop
+ * @author	   Cropper.js by Fengyuan Chen <http://chenfengyuan.com>       
+ * @author	   based on the extension Moo_imagecropper by Lightive (erwan.ripoll)       
+ * @license    GPL 
+ * 
+ */
+
+
+/**
+ * Intro
+ */
+$GLOBALS['TL_LANG']['MSC']['imagecropHeadline'] = 'Recadrer l\'image';
+$GLOBALS['TL_LANG']['MSC']['imagecropIntroduction'] = 'Utilisez les marqueurs sur la bordure pour définir la zone de cadrage.';
+
+/**
+ * Section headlines
+ */
+$GLOBALS['TL_LANG']['MSC']['imagecropData'] = 'Données';
+$GLOBALS['TL_LANG']['MSC']['imagecropPreview'] = 'Aperçu';
+$GLOBALS['TL_LANG']['MSC']['imagecropAspectRatio'] = 'Ratios';
+$GLOBALS['TL_LANG']['MSC']['imagecropSizes'] = 'Tailles de pixel';
+$GLOBALS['TL_LANG']['MSC']['imagecropTools'] = 'Outils';
+
+/**
+ * Section data
+ */
+$GLOBALS['TL_LANG']['MSC']['imagecropWidth'] = 'Largeur';
+$GLOBALS['TL_LANG']['MSC']['imagecropHeight'] = 'Hauteur';
+
+/**
+ * Submit buttons
+ */
+$GLOBALS['TL_LANG']['MSC']['imagecropUpdateImage'] = 'Sauvegarder';
+$GLOBALS['TL_LANG']['MSC']['imagecropSaveCopy'] = 'Sauvegarder dans une nouvelle version';
+
+/**
+ * Credits
+ */
+$GLOBALS['TL_LANG']['MSC']['imagecropCredits'] = 'Cet extension utilise la librairie Javascript <a href="https://fengyuanchen.github.io/cropperjs/">Cropper.js</a> de <a href="http://chenfengyuan.com">Fengyuan Chen.</a>';
+
+/**
+ * Tooltips
+ */
+$GLOBALS['TL_LANG']['MSC']['imagecropToolMove'] = 'Déplacez l\'image ou rognez le cadre';
+$GLOBALS['TL_LANG']['MSC']['imagecropToolCrop'] = 'Déplacer ou recréez le cadre';
+$GLOBALS['TL_LANG']['MSC']['imagecropToolZoomIn'] = 'Zoomez l\'image';
+$GLOBALS['TL_LANG']['MSC']['imagecropToolZoomOut'] = 'Dezoomez l\'image';
+$GLOBALS['TL_LANG']['MSC']['imagecropToolMoveLeft'] = 'Déplacez l\'image de 10px vers la droite';
+$GLOBALS['TL_LANG']['MSC']['imagecropToolMoveRight'] = 'Déplacez l\'image de 10px vers la gauche';
+$GLOBALS['TL_LANG']['MSC']['imagecropToolMoveDown'] = 'Déplacez l\'image de 10px vers le bas';
+$GLOBALS['TL_LANG']['MSC']['imagecropToolMoveUp'] = 'Déplacez l\'image de 10px vers le haut';
+$GLOBALS['TL_LANG']['MSC']['imagecropToolReset'] = 'Réinitialiser';
+
+?>
